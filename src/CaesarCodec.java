2a637c2fe00d65530d9a81131e2beac0bb549b6d
Force_human <66125890+Ffferrar@users.noreply.github.com>
2021-11-04 21:22:49 +0300

    Procurement

---
+++ src/CaesarCodec.java
@@ -0,0 +1,19 @@
+public class CaesarCodec implements Coder {
+    int key;
+
+    public CaesarCodec(int key){
+        this.key = key;
+    }
+
+    public String decode(String input) { //расшифровка
+        return null;
+    }
+
+    public String encode(String input) { //шифровка
+        return null;
+    }
+
+    public static void main(String[] args) {
+        //System.out.println((int)'Z');
+    }
+}
